5b5fe4c984d83c9d194fa1e647d1155e997dfcde
AkylTorgoev <akyltorgoev46@gmail.com>
2022-05-23 17:49:31 +0600

    .

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PaymentForm from "../components/Payment/PaymentForm";
 
 const PaymentPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PaymentForm />
+    </div>
+  );
 };
 
 export default PaymentPage;
